49f48eb0527bb970c6c29445ff2ceb1b0b6e5f42
Briancgmendoza <briancgmendoza@gmail.com>
2023-10-06 11:12:30 +0800

    saving to github

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import router from "./router";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use('/', router());
